615553ec9755815368b7bbbd332df8f6682d07ea
sfusco <slf@stevenfusco.com>
2025-10-04 15:08:54 -0400

    updates for react native 0.81.4

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-module.exports = require('./lib/electrum_client');
+const client = require('./lib/electrum_client');
+const helpers = require('./helpers/index');
+
+module.exports = client;
+module.exports.helpers = helpers;
